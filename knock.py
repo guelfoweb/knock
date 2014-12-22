6d2e3cee0e1bad6f9183dd829a5b612a380363e7
wisp <wisprp@gmail.com>
2014-12-22 10:36:15 +0300

    Fixed TabError: inconsistent use of tabs and spaces in indentation in Python 3

--- knock.py
+++ knock.py
@@ -20,303 +20,315 @@ import sys
 sys.path.insert(0, 'modules')
 
 try:
-		import knockcore
+    import knockcore
 except ImportError:
-		print('[!] knockcore not found.')
-		sys.exit(0)
+    print('[!] knockcore not found.')
+    sys.exit(0)
 
 # INFORMATION
-NAME		= "Knock Subdomain Scan"
-VERSION		= "2.0"
-AUTHOR		= "Author: Gianni 'guelfoweb' Amato"
-GITHUB		= "Github: https://github.com/guelfoweb/knock"
-INFO		= NAME+" v."+VERSION+" - Open Source Project\n"+AUTHOR+"\n"+GITHUB
+NAME = "Knock Subdomain Scan"
+VERSION = "2.0"
+AUTHOR = "Author: Gianni 'guelfoweb' Amato"
+GITHUB = "Github: https://github.com/guelfoweb/knock"
+INFO = NAME + " v." + VERSION + " - Open Source Project\n" + AUTHOR + "\n" + GITHUB
 
 # color const
 COLOR_ALIAS = '\033[93m'
-COLOR_RED   = '\033[91m'
-COLOR_BOLD  = '\033[1m'
-COLOR_END   = '\033[0m'
+COLOR_RED = '\033[91m'
+COLOR_BOLD = '\033[1m'
+COLOR_END = '\033[0m'
+
 
 def uniq_by_array(array):
-	uniq_ip   = []
-	uniq_name = []
-	array_row = len(array)
-	for i in range(0,array_row):
-		a  = array[i][0]
-		b  = array[i][1]
-		if a not in uniq_ip:
-			uniq_ip.append(a)
-		if b not in uniq_name:
-			uniq_name.append(b)
+    uniq_ip = []
+    uniq_name = []
+    array_row = len(array)
+    for i in range(0, array_row):
+        a = array[i][0]
+        b = array[i][1]
+        if a not in uniq_ip:
+            uniq_ip.append(a)
+        if b not in uniq_name:
+            uniq_name.append(b)
 
-	print(COLOR_BOLD + "Ip Addr Summary" + COLOR_END)
-	print("-"*15)
-	for i in range(0,len(uniq_ip)):
-		print(uniq_ip[i])
+    print(COLOR_BOLD + "Ip Addr Summary" + COLOR_END)
+    print("-" * 15)
+    for i in range(0, len(uniq_ip)):
+        print(uniq_ip[i])
 
-	host_found = len(uniq_ip)
-	name_found = len(uniq_name)
-	print("\nFound "+str(name_found)+" subdomain(s) in "+str(host_found)+" host(s).")
-	
+    host_found = len(uniq_ip)
+    name_found = len(uniq_name)
+    print("\nFound " + str(name_found) + " subdomain(s) in " + str(host_found) + " host(s).")
+
 
 def resolvedomain(subdomain):
-	result    = knockcore.domaininfo(subdomain)
+    result = knockcore.domaininfo(subdomain)
 
-#		 [ 4 x 3 ]								[ 3 x 3 ]
-#		 [['hostname'],                0
-#		  ['alias', 'alias', 'ip'],    1 0
-#		  ['alias', 'alias', 'ip'],    2 1
-#		  ['alias', 'alias', 'ip']]    3 2
-#		      0        1      2
+    # [ 4 x 3 ]								[ 3 x 3 ]
+    #		 [['hostname'],                0
+    #		  ['alias', 'alias', 'ip'],    1 0
+    #		  ['alias', 'alias', 'ip'],    2 1
+    #		  ['alias', 'alias', 'ip']]    3 2
+    #		      0        1      2
 
-#		 [ 3 x 3 ]								[ 2 x 3 ]
-#		 [['login.lga1.b.yahoo.com'], 														  0
-#		  ['login.yahoo.com',               'l2.login.vip.bf1.yahoo.com', '98.139.237.162'],  1 0
-#		  ['login-global.lgg1.b.yahoo.com', 'l2.login.vip.bf1.yahoo.com', '98.139.237.162']]  2 1
-#		                 0                                 1                       2
+    #		 [ 3 x 3 ]								[ 2 x 3 ]
+    #		 [['login.lga1.b.yahoo.com'], 														  0
+    #		  ['login.yahoo.com',               'l2.login.vip.bf1.yahoo.com', '98.139.237.162'],  1 0
+    #		  ['login-global.lgg1.b.yahoo.com', 'l2.login.vip.bf1.yahoo.com', '98.139.237.162']]  2 1
+    #		                 0                                 1                       2
 
-	# subdomain is alias
-	if result and len(result) == 2 and len(result[1]) == 2:
-		hostname = result[0][0]
-		alias    = result[1][0]
-		ip       = result[1][1]
-		print(COLOR_ALIAS + ip+"\t"+alias + COLOR_END)
-		print(ip+"\t"+hostname)
-		found.append([ip, alias])
-		found.append([ip, hostname])
-	# subdomain is alias
-	elif result and len(result) == 2 and len(result[1]) == 3:
-		hostname = result[0][0]
-		alias    = result[1][0]
-		name     = result[1][1]
-		ip       = result[1][2]
-		print(COLOR_ALIAS + ip+"\t"+alias + COLOR_END)
-		print(ip+"\t"+hostname)
-		found.append([ip, alias])
-		found.append([ip, hostname])
-	# subdomain is alias
-	elif result and not len(result) == 2 and not False:
-		uniq = []
-		for i in range(1,len(result)):
-			ip_alias = result[i][2]+"\t"+result[i][0]
-			ip_name  = result[i][2]+"\t"+result[i][1]
-			
-			if ip_alias not in uniq:
-				uniq.append(ip_alias)
-				print(COLOR_ALIAS + ip_alias + COLOR_END)
-				found.append([result[i][2], result[i][0]])
-			if ip_name not in uniq:
-				uniq.append(ip_name)
-				print(ip_name)
-				found.append([result[i][2], result[i][1]])
-		for i in range(1,len(result)):
-			ip_hostname  = result[i][2]+"\t"+result[0][0]
-			if ip_hostname not in uniq:
-				uniq.append(ip_hostname)
-				print(ip_hostname)
-				found.append([result[i][2], result[0][0]])
-	# subdomain is hostname
-	elif result:
-		hostname = result[0][0]
-		ip       = result[1][0]
-		print(ip+"\t"+hostname)
-		found.append([ip, hostname])
+    # subdomain is alias
+    if result and len(result) == 2 and len(result[1]) == 2:
+        hostname = result[0][0]
+        alias = result[1][0]
+        ip = result[1][1]
+        print(COLOR_ALIAS + ip + "\t" + alias + COLOR_END)
+        print(ip + "\t" + hostname)
+        found.append([ip, alias])
+        found.append([ip, hostname])
+    # subdomain is alias
+    elif result and len(result) == 2 and len(result[1]) == 3:
+        hostname = result[0][0]
+        alias = result[1][0]
+        name = result[1][1]
+        ip = result[1][2]
+        print(COLOR_ALIAS + ip + "\t" + alias + COLOR_END)
+        print(ip + "\t" + hostname)
+        found.append([ip, alias])
+        found.append([ip, hostname])
+    # subdomain is alias
+    elif result and not len(result) == 2 and not False:
+        uniq = []
+        for i in range(1, len(result)):
+            ip_alias = result[i][2] + "\t" + result[i][0]
+            ip_name = result[i][2] + "\t" + result[i][1]
+
+            if ip_alias not in uniq:
+                uniq.append(ip_alias)
+                print(COLOR_ALIAS + ip_alias + COLOR_END)
+                found.append([result[i][2], result[i][0]])
+            if ip_name not in uniq:
+                uniq.append(ip_name)
+                print(ip_name)
+                found.append([result[i][2], result[i][1]])
+        for i in range(1, len(result)):
+            ip_hostname = result[i][2] + "\t" + result[0][0]
+            if ip_hostname not in uniq:
+                uniq.append(ip_hostname)
+                print(ip_hostname)
+                found.append([result[i][2], result[0][0]])
+    # subdomain is hostname
+    elif result:
+        hostname = result[0][0]
+        ip = result[1][0]
+        print(ip + "\t" + hostname)
+        found.append([ip, hostname])
+
 
 def loadwordlist(wordlist):
-	wlist = knockcore.loadfile(wordlist)
-	if wlist == False:
-		print(COLOR_RED + "\nFile not found ["+wordlist+"]" + COLOR_END)
-		print("Download a wordlist file from here:")
-		print("https://raw.github.com/guelfoweb/knock/master/wordlist.txt")
-		sys.exit(0)
-	return wlist
+    wlist = knockcore.loadfile(wordlist)
+    if wlist == False:
+        print(COLOR_RED + "\nFile not found [" + wordlist + "]" + COLOR_END)
+        print("Download a wordlist file from here:")
+        print("https://raw.github.com/guelfoweb/knock/master/wordlist.txt")
+        sys.exit(0)
+    return wlist
+
 
 def subscan(url, wordlist):
-	wlist = loadwordlist(wordlist)
+    wlist = loadwordlist(wordlist)
 
-	print(COLOR_BOLD + "Getting subdomain for", url + COLOR_END)
-	print("\nIp Address\tDomain Name")
-	print("----------\t-----------")
+    print(COLOR_BOLD + "Getting subdomain for", url + COLOR_END)
+    print("\nIp Address\tDomain Name")
+    print("----------\t-----------")
 
-	for sub in wlist:
-		subdomain = sub+"."+url
-		resolvedomain(subdomain)
+    for sub in wlist:
+        subdomain = sub + "." + url
+        resolvedomain(subdomain)
 
-	print()
-	uniq_by_array(found)
+    print()
+    uniq_by_array(found)
+
 
 def bypasswildcard(url, wordlist):
-	wlist = loadwordlist(wordlist)
+    wlist = loadwordlist(wordlist)
 
-	print(COLOR_BOLD + "\nGetting subdomain for", url + COLOR_END)
-	print("\nIp Address\tDomain Name")
-	print("----------\t-----------")
+    print(COLOR_BOLD + "\nGetting subdomain for", url + COLOR_END)
+    print("\nIp Address\tDomain Name")
+    print("----------\t-----------")
 
-	for sub in wlist:
-		subdomain = sub+"."+url
-		header = knockcore.getheader(subdomain, "/", "GET")
-		# bypass status code -> header[0] = 301
-		if header and not header[0] == 301:
-			resolvedomain(subdomain)
+    for sub in wlist:
+        subdomain = sub + "." + url
+        header = knockcore.getheader(subdomain, "/", "GET")
+        # bypass status code -> header[0] = 301
+        if header and not header[0] == 301:
+            resolvedomain(subdomain)
 
-	print()
-	uniq_by_array(found)
+    print()
+    uniq_by_array(found)
+
 
 def checkzone(domain):
-	print( COLOR_BOLD + "Getting NS records for", domain + COLOR_END)
-	print("\nIp Address\tServer Name")
-	print("----------\t-----------")
-	zt_found = knockcore.zonetransfer(url)
-	if (zt_found):
-		print(COLOR_BOLD + "Getting Zone Transfer\n" + COLOR_END)
-		print("Ip Address\tDomain Name")
-		print("----------\t-----------")
-		for sub in zt_found:
-			resolvedomain(sub)
-		print()
-	else:
-		return False
+    print(COLOR_BOLD + "Getting NS records for", domain + COLOR_END)
+    print("\nIp Address\tServer Name")
+    print("----------\t-----------")
+    zt_found = knockcore.zonetransfer(url)
+    if (zt_found):
+        print(COLOR_BOLD + "Getting Zone Transfer\n" + COLOR_END)
+        print("Ip Address\tDomain Name")
+        print("----------\t-----------")
+        for sub in zt_found:
+            resolvedomain(sub)
+        print()
+    else:
+        return False
+
 
 def getheaders(url, path, method):
-	# Status -> header[0] 
-	# Reason -> header[1]
-	# Header -> header[2]
-	header = knockcore.getheader(url, path, method)
-	status = str(header[0])
-	reason = str(header[1])
-	print(COLOR_BOLD + "Staus\tReason" +  COLOR_END)
-	print("-----\t------")
-	print(status + "\t" + reason)
-	print()
-	print(COLOR_BOLD + "Response Headers" +  COLOR_END)
-	print("-"*16)
-	for i in range(1,len(header[2])):
-		print(str(header[2][i][0]) + ": " + str(header[2][i][1]))
-	return status, reason
+    # Status -> header[0]
+    # Reason -> header[1]
+    # Header -> header[2]
+    header = knockcore.getheader(url, path, method)
+    status = str(header[0])
+    reason = str(header[1])
+    print(COLOR_BOLD + "Staus\tReason" + COLOR_END)
+    print("-----\t------")
+    print(status + "\t" + reason)
+    print()
+    print(COLOR_BOLD + "Response Headers" + COLOR_END)
+    print("-" * 16)
+    for i in range(1, len(header[2])):
+        print(str(header[2][i][0]) + ": " + str(header[2][i][1]))
+    return status, reason
+
 
 def CheckForWildcard(url):
-	# test wildcard and return True or False
-	wildcard  = knockcore.testwildcard(url)
-	
-	if wildcard == False:
-		return False
-	else:
-		print(COLOR_RED+COLOR_BOLD+"Wildcard enabled\n"+COLOR_END)
+    # test wildcard and return True or False
+    wildcard = knockcore.testwildcard(url)
+
+    if wildcard == False:
+        return False
+    else:
+        print(COLOR_RED + COLOR_BOLD + "Wildcard enabled\n" + COLOR_END)
+
 
 def check_status(url, path, method):
-	try:
-		header   = knockcore.getheader(url, path, method)
-		status   = str(header[0])
-		reason   = str(header[1])
-		response = header[2]
-		return status, reason, response
-	except:
-		sys.exit(0)
+    try:
+        header = knockcore.getheader(url, path, method)
+        status = str(header[0])
+        reason = str(header[1])
+        response = header[2]
+        return status, reason, response
+    except:
+        sys.exit(0)
+
 
 def purgeurl(url):
-	url = url.replace("http://","")
-	url = url.replace("/","")
-	return url
+    url = url.replace("http://", "")
+    url = url.replace("/", "")
+    return url
+
 
 def help():
-	print(COLOR_BOLD+INFO+COLOR_END)
-	print()
-	print(COLOR_BOLD+"Usage:"+COLOR_END+" knock.py domain.com")
-	print(COLOR_BOLD+"Usage:"+COLOR_END+" knock.py domain.com "+COLOR_BOLD+"--worlist "+COLOR_END+"wordlist.txt")
-	print("\n\t-h, --help\tThis help")
-	print("\t-v, --version\tShow version")
-	print("\t    --wordlist\tUse personal wordlist")
-	print(COLOR_BOLD+"\nOptions for single domain"+COLOR_END)
-	print("-"*25)
-	print("\t-i, --info\tShort information")
-	print("\t-r, --resolve\tResolve domain name")
-	print("\t-w, --wildcard\tCheck if wildcard is enabled")
-	print("\t-z, --zone\tCheck if Zone Transfer is enabled")
-#	print("\t    --get\tRequest HTTP for GET method")
-#	print("\t    --post\tRequest HTTP for POST method")
-#	print("\t    --head\tRequest HTTP for HEAD method")
-#	print("\t    --trace\tRequest HTTP for TRACE method")
-#	print("\t    --options\tRequest HTTP for OPTIONS method")
-	print("\n"+" "*9+COLOR_BOLD+"Usage:"+COLOR_END+" knock.py"+COLOR_BOLD+" [-opt, --option]"+COLOR_END+" domain.com")
-	print("\nNote: The ALIAS name is marked in yellow.")
-	sys.exit(0)
+    print(COLOR_BOLD + INFO + COLOR_END)
+    print()
+    print(COLOR_BOLD + "Usage:" + COLOR_END + " knock.py domain.com")
+    print(
+        COLOR_BOLD + "Usage:" + COLOR_END + " knock.py domain.com " + COLOR_BOLD + "--worlist " + COLOR_END + "wordlist.txt")
+    print("\n\t-h, --help\tThis help")
+    print("\t-v, --version\tShow version")
+    print("\t    --wordlist\tUse personal wordlist")
+    print(COLOR_BOLD + "\nOptions for single domain" + COLOR_END)
+    print("-" * 25)
+    print("\t-i, --info\tShort information")
+    print("\t-r, --resolve\tResolve domain name")
+    print("\t-w, --wildcard\tCheck if wildcard is enabled")
+    print("\t-z, --zone\tCheck if Zone Transfer is enabled")
+    # print("\t    --get\tRequest HTTP for GET method")
+    #	print("\t    --post\tRequest HTTP for POST method")
+    #	print("\t    --head\tRequest HTTP for HEAD method")
+    #	print("\t    --trace\tRequest HTTP for TRACE method")
+    #	print("\t    --options\tRequest HTTP for OPTIONS method")
+    print(
+        "\n" + " " * 9 + COLOR_BOLD + "Usage:" + COLOR_END + " knock.py" + COLOR_BOLD + " [-opt, --option]" + COLOR_END + " domain.com")
+    print("\nNote: The ALIAS name is marked in yellow.")
+    sys.exit(0)
 
-#___________________Main_____________________
+# ___________________Main_____________________
 
 global found
 found = []
 global wlist
 
 if len(sys.argv) == 1 or len(sys.argv) > 4:
-	help()
+    help()
 
 if len(sys.argv) == 2:
-	url = sys.argv[1]
-	url = purgeurl(url)
-	if url == "-h" or url == "--help":
-		help()
-	if url == "-v" or url == "--version":
-		print(VERSION)
-		sys.exit(0)
-	if CheckForWildcard(url) == False:
-		checkzone(url)
-		subscan(url, "wordlist.txt")
-	else:
-		checkzone(url)
-		getheaders(url, "/", "GET")
-		bypasswildcard(url, "wordlist.txt")
-	sys.exit(0)
+    url = sys.argv[1]
+    url = purgeurl(url)
+    if url == "-h" or url == "--help":
+        help()
+    if url == "-v" or url == "--version":
+        print(VERSION)
+        sys.exit(0)
+    if CheckForWildcard(url) == False:
+        checkzone(url)
+        subscan(url, "wordlist.txt")
+    else:
+        checkzone(url)
+        getheaders(url, "/", "GET")
+        bypasswildcard(url, "wordlist.txt")
+    sys.exit(0)
 
 if len(sys.argv) == 3:
-	opt = sys.argv[1]
-	url = sys.argv[2]
-	url = purgeurl(url)
-	if opt == "-i" or opt == "--info":
-		print(COLOR_BOLD + "Resolving domain", url + "\n" + COLOR_END)
-		resolvedomain(url)
-		print()
-		if checkzone(url) == False:
-			print("Zone Transfer not enabled\n")
-		CheckForWildcard(url)
-		getheaders(url, "/", "GET")
+    opt = sys.argv[1]
+    url = sys.argv[2]
+    url = purgeurl(url)
+    if opt == "-i" or opt == "--info":
+        print(COLOR_BOLD + "Resolving domain", url + "\n" + COLOR_END)
+        resolvedomain(url)
+        print()
+        if checkzone(url) == False:
+            print("Zone Transfer not enabled\n")
+        CheckForWildcard(url)
+        getheaders(url, "/", "GET")
 
-	if opt == "-r" or opt == "--resolve":
-		print(COLOR_BOLD + "Resolving domain", url + "\n" + COLOR_END)
-		resolvedomain(url)
-	if opt == "-w" or opt == "--wildcard":
-		CheckForWildcard(url)
-	if opt == "-z" or opt == "--zone":
-		if checkzone(url) == False:
-			print("Zone Transfer not enabled")
+    if opt == "-r" or opt == "--resolve":
+        print(COLOR_BOLD + "Resolving domain", url + "\n" + COLOR_END)
+        resolvedomain(url)
+    if opt == "-w" or opt == "--wildcard":
+        CheckForWildcard(url)
+    if opt == "-z" or opt == "--zone":
+        if checkzone(url) == False:
+            print("Zone Transfer not enabled")
 
-	# ------- hidden for debug -------
-	if opt == "--trace":
-		getheaders(url, "/", "TRACE")
-	if opt == "--get":
-		getheaders(url, "/", "GET")
-	if opt == "--post":
-		getheaders(url, "/", "POST")
-	if opt == "--head":
-		getheaders(url, "/", "HEAD")
-	if opt == "--options":
-		getheaders(url, "/", "OPTIONS")
-	# --------------------------------
+    # ------- hidden for debug -------
+    if opt == "--trace":
+        getheaders(url, "/", "TRACE")
+    if opt == "--get":
+        getheaders(url, "/", "GET")
+    if opt == "--post":
+        getheaders(url, "/", "POST")
+    if opt == "--head":
+        getheaders(url, "/", "HEAD")
+    if opt == "--options":
+        getheaders(url, "/", "OPTIONS")
+    # --------------------------------
 
-	sys.exit(0)
+    sys.exit(0)
 
 if len(sys.argv) == 4:
-	url   = sys.argv[1]
-	url   = purgeurl(url)
-	opt   = sys.argv[2]
-	wlist = sys.argv[3]
-	if opt == "--wordlist":
-		if CheckForWildcard(url) == False:
-			checkzone(url)
-			subscan(url, wlist)
-		else:
-			checkzone(url)
-			getheaders(url, "/", "GET")
-			bypasswildcard(url, wlist)
-	sys.exit(0)
+    url = sys.argv[1]
+    url = purgeurl(url)
+    opt = sys.argv[2]
+    wlist = sys.argv[3]
+    if opt == "--wordlist":
+        if CheckForWildcard(url) == False:
+            checkzone(url)
+            subscan(url, wlist)
+        else:
+            checkzone(url)
+            getheaders(url, "/", "GET")
+            bypasswildcard(url, wlist)
+    sys.exit(0)
 
--- modules/knockcore.py
+++ modules/knockcore.py
@@ -30,126 +30,134 @@ else:
 
 # set the default timeout on sockets to 5 seconds
 if hasattr(socket, 'setdefaulttimeout'):
-	socket.setdefaulttimeout(5) # <- here
+    socket.setdefaulttimeout(5)  # <- here
+
 
 def domaininfo(domain):
-	try:
-		# translate a host name to IPv4 address format
-		host = socket.gethostbyname(domain)
-	except:
-		pass
-		return False
-		
-	if host:
-		# return a triple (hostname, aliaslist, ipaddrlist) by HOST
-		soc      = socket.gethostbyname_ex(domain)
-#		print(soc) # enable for debug
-		elem     = []
-		hostname = soc[0]
-		ip       = soc[2][0]
+    try:
+        # translate a host name to IPv4 address format
+        host = socket.gethostbyname(domain)
+    except:
+        pass
+        return False
 
-		elem.append([hostname])
+    if host:
+        # return a triple (hostname, aliaslist, ipaddrlist) by HOST
+        soc = socket.gethostbyname_ex(domain)
+        # print(soc) # enable for debug
+        elem = []
+        hostname = soc[0]
+        ip = soc[2][0]
 
-		# verbose
-		len_alias_array = len(soc[1])
-		if len_alias_array > 0:
-			for i in range(0, len_alias_array):
-				alias = soc[1][i]
-				try:
-					# Return a triple (hostname, aliaslist, ipaddrlist) by IP
-					name  = socket.gethostbyaddr(soc[1][i])[0]
-					ip    = socket.gethostbyname(name)
-				except:
-					name  = alias
-					ip    = socket.gethostbyname(alias)
-					pass
-					
-				elem.append([alias, name, ip]) 
-		else:
-			elem.append([ip])
-		
-		return (elem)
-		soc.close()
+        elem.append([hostname])
+
+        # verbose
+        len_alias_array = len(soc[1])
+        if len_alias_array > 0:
+            for i in range(0, len_alias_array):
+                alias = soc[1][i]
+                try:
+                    # Return a triple (hostname, aliaslist, ipaddrlist) by IP
+                    name = socket.gethostbyaddr(soc[1][i])[0]
+                    ip = socket.gethostbyname(name)
+                except:
+                    name = alias
+                    ip = socket.gethostbyname(alias)
+                    pass
+
+                elem.append([alias, name, ip])
+        else:
+            elem.append([ip])
+
+        return (elem)
+        soc.close()
 
 
-def zonetransfer(URL): # Zone Transfer
-	try:
-		import dns.query, dns.zone, dns.resolver
-		answers = dns.resolver.query(URL, 'NS')
-		ns = []
-		for rdata in answers:
-			n        = str(rdata)
-			ns_array = domaininfo(n)
-			print(str(ns_array[1][0]) + "\t" + str(ns_array[0][0]))
-			ns.append(n)
+def zonetransfer(URL):  # Zone Transfer
+    try:
+        import dns.query, dns.zone, dns.resolver
 
-		print()
+        answers = dns.resolver.query(URL, 'NS')
+        ns = []
+        for rdata in answers:
+            n = str(rdata)
+            ns_array = domaininfo(n)
+            print(str(ns_array[1][0]) + "\t" + str(ns_array[0][0]))
+            ns.append(n)
 
-		for n in ns:
-			zt = []
-			try:
-			  zone = dns.zone.from_xfr(dns.query.xfr(ns[0], URL))
-			  for name, node in zone.nodes.items():
-			      rdataset = node.rdatasets
-			      for record in rdataset:
-					if not str(name) == "@" and not str(name) == "*":
-						zt.append(str(name) + "." + URL)
-			  return zt
-#			          print, "\n%s\t%s" % (name, record)
-			  #break
-			  print()
-			except:
-				return False
-				pass
-	except:
-		return False
+        print()
+
+        for n in ns:
+            zt = []
+            try:
+                zone = dns.zone.from_xfr(dns.query.xfr(ns[0], URL))
+                for name, node in zone.nodes.items():
+                    rdataset = node.rdatasets
+                    for record in rdataset:
+                        if not str(name) == "@" and not str(name) == "*":
+                            zt.append(str(name) + "." + URL)
+                return zt
+                # print, "\n%s\t%s" % (name, record)
+                #break
+                print()
+            except:
+                return False
+                pass
+    except:
+        return False
+
 
 def loadfile(filename):
-	try:
-		filename = open(filename,'r')
-		wlist = filename.read().split('\n')
-		filename.close
-		return wlist
-	except:
-		return False
+    try:
+        filename = open(filename, 'r')
+        wlist = filename.read().split('\n')
+        filename.close
+        return wlist
+    except:
+        return False
 
-def rnd(): # random string
-	alphabet  = 'abcdefghijklmnopqrstuvwxyz'
-	min       = 5
-	max       = 15
-	total     = 2
-	rndstring = ''
-	for count in xrange(1,total):
-		for x in random.sample(alphabet,random.randint(min,max)):
-			rndstring+=x
-	return rndstring
-	
+
+def rnd():  # random string
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    min = 5
+    max = 15
+    total = 2
+    rndstring = ''
+    for count in xrange(1, total):
+        for x in random.sample(alphabet, random.randint(min, max)):
+            rndstring += x
+    return rndstring
+
+
 def testwildcard(url):
-	rndString = rnd() # get random string
-	subdomain = rndString+"."+url
-	try:
-		host  = socket.gethostbyname(subdomain)
-		return True
-	except:
-		return False
+    rndString = rnd()  # get random string
+    subdomain = rndString + "." + url
+    try:
+        host = socket.gethostbyname(subdomain)
+        return True
+    except:
+        return False
+
 
 agent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_6; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.237 Safari/534.10"
+
+
 def getheader(url, path, method):
-	try:
-		conn = httplib.HTTPConnection(url)
-		conn.putrequest(method, path)
-		conn.putheader("User-Agent", agent)
-		# Test for XST
-#		conn.putheader("Via", "<script>alert(0)</script>")
-		conn.endheaders()
-#		conn.request(method, path)
-		res = conn.getresponse()
-		return res.status, res.reason, res.getheaders()
-		conn.close()
-	except:
-		return False
+    try:
+        conn = httplib.HTTPConnection(url)
+        conn.putrequest(method, path)
+        conn.putheader("User-Agent", agent)
+        # Test for XST
+        # conn.putheader("Via", "<script>alert(0)</script>")
+        conn.endheaders()
+        #		conn.request(method, path)
+        res = conn.getresponse()
+        return res.status, res.reason, res.getheaders()
+        conn.close()
+    except:
+        return False
 
 # for debug
-#print(zonetransfer("zonetransfer.me"))
+# print(zonetransfer("zonetransfer.me"))
 #print(getheader("egfrgfwr.reddit.com"))
 #subscan("google.com", "wordlist.txt")
