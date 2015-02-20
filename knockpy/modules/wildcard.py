844702b42d2f33e16c98b348d3937b1eaa444035
Gianni Amato <guelfoweb@gmail.com>
2015-02-20 17:15:17 +0100

    Create wildcard.py

---
+++ knockpy/modules/wildcard.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# ----------------------------------------------------------------------
+# This file is part of Knock subdomain scan (aka knockpy)
+#
+# Knock is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Knock is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Knock. If not, see <http://www.gnu.org/licenses/>.
+# ----------------------------------------------------------------------
+
+import getheader
+import random
+import socket
+
+def bypass(target, code):
+	# beta feature
+	text = ''
+	header = getheader.req(target,'/','HEAD')
+	# bypass status code -> header[0] = 301 or 302
+	if header and header[0] != code: return True
+	else: return False
+
+def test_random(target):
+	rndString = rnd('abcdefghijklmnopqrstuvwxyz')
+	rndSubdomain = str(rndString)+'.'+target
+	try:
+		host  = socket.gethostbyname(rndSubdomain)
+		if host:
+			return True
+	except:
+		return False
+
+def test(target):
+	if test_random(target) and getheader.req(target,'/','HEAD'): return True
+	return False
+
+def rnd(alphabet): # random string
+	alphabet  = 'abcdefghijklmnopqrstuvwxyz'
+	min       = 5
+	max       = 15
+	total     = 2
+	rndstring = ''
+	for count in xrange(1,total):
+		for x in random.sample(alphabet,random.randint(min,max)):
+			rndstring+=x
+	return rndstring
