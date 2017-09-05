5f81515910fcbbbdb6fb1dc2aa9c3609822e3d7f
Gianni Amato <guelfoweb@gmail.com>
2017-09-05 15:05:28 +0200

    added -f or --csvfileds option
    add fields name to the first row of csv output file

--- knockpy/modules/save_report.py
+++ knockpy/modules/save_report.py
@@ -6,11 +6,13 @@ def touch(filename):
 	file = open(fname, 'w')
 	file.close()
 
-def export(domain, report, _type):
+def export(domain, report, _type, fields=False):
 	timestamp = time.time()
 	filename = domain.replace('.', '_')+'_'+str(timestamp)+'.'+_type
 	if _type == 'csv':
 		csv_report = ''
+		if fields:
+			csv_report += 'ip,status,type,domain_name,server\n'
 		for item in report:
 			csv_report += item + '\n'
 		report = csv_report
